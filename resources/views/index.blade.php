654083ead8f88a6fe3eb30cb39e0b12634ea2239
Gunturris <guntur@asi-asiapacific.com>
2024-09-14 22:56:52 +0700

    rid

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,6 +42,7 @@
             <img class="img-badak" src="/img/badak.png" alt="">
         </div>
         <img class="main-logo w-100" src="/img/main-logo.png" alt="">
+        <div class="d-flex align-items-center justify-content-between">
         <div class="prod-ds">
             <div class="box-lg"></div>
             <img src="/img/product-1.png" alt="">
@@ -51,6 +52,18 @@
                     <button class="w-100 btn btn-light-green">Isi Form</button>
                 </a>
             </div>
+        </div>
+
+        <div class="prod-ds">
+            <div class="box-lg"></div>
+            <img src="/img/product-1.png" alt="">
+            <div class="box-white">
+                <span class="product-nm">Larutan Penyegar Cap Badak Legacy</span>
+                <a class="w-100" href="/form">
+                    <button class="w-100 btn btn-light-green">Isi Form</button>
+                </a>
+            </div>
+        </div>
         </div>
         <div class="copyright">© 2024 LARUTAN PENYEGAR CAP BADAK. ALL RIGHTS RESERVED.</div>
     </div>
